ff8e2ee627a0652fc66a7eddb241b8046c146de5
HCS Learning <learning@hardcybersoft.cl>
2020-03-17 01:48:55 -0300

    last day of datetime

--- datetime.php
+++ datetime.php
@@ -4,6 +4,12 @@ $d = new DateTime('2020-01-15');
 echo $d->format('Y-m-d')."\n\n";
 
 
-echo "DateTime sin argumentos\n";
+echo "Datetime sin argumentos\n";
 $d = new DateTime();
-echo $d->format('Y-m-d')."\n";
+echo $d->format('Y-m-d')."\n\n";
+
+echo "Datetime en palabras \n";
+echo (new Datetime('last day of this month'))->format('Y-m-d')."\n";
+echo (new Datetime('last day of 2020-02'))->format('Y-m-d')."\n";
+echo"\n\n";
+
