396189f8d2514b0dad43b6aa8a180e2ffbdad7af
HCS Learning <learning@hardcybersoft.cl>
2020-04-20 05:36:34 -0400

    last day of -- funciona para el mes actual configurado en la fecha DateTime

--- datetime.php
+++ datetime.php
@@ -11,5 +11,12 @@ echo $d->format('Y-m-d')."\n\n";
 echo "Datetime en palabras \n";
 echo (new Datetime('last day of this month'))->format('Y-m-d')."\n";
 echo (new Datetime('last day of 2020-02'))->format('Y-m-d')."\n";
-echo"\n\n";
+echo "\n\n";
+
+echo "Creación de DateTime from String\n";
+$fecha = DateTime::createFromFormat('Ym', '202003');
+$fecha->modify('last day of');
+echo $fecha->format('Y-m-d');
+echo "\n\n";
+
 
